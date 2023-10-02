3267c99a10b774e3e21c506cccf5144ed610be1c
Madhukar Bhaktha <madzb.programming@gmail.com>
2023-10-02 18:12:20 +0530

    02_03-theFirstProgram

--- 01-IntroductionToProgramming/Application1.java
+++ 01-IntroductionToProgramming/Application1.java
@@ -1,6 +1,7 @@
 public class Application1{
-    public static void main ( String []args ){
+    public static void main (String[]args){
         System.out.print("Hello");
+        System.out.println("Hello World");
     }
 }
 
