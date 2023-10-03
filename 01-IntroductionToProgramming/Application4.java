eb5ae64f84f775f2fbb88be04ba7ebad3a901c72
Madhukar Bhaktha <madzb.programming@gmail.com>
2023-10-03 20:40:02 +0530

    02_10-methods

---
+++ 01-IntroductionToProgramming/Application4.java
@@ -0,0 +1,27 @@
+public class Application4 {
+    public static void main(String[] args){
+
+
+        // calc.firstNumber = 10;
+        int sumOfCalc =Calculator1.add(10, 8);
+        System.out.println(sumOfCalc);
+
+
+        // calc1.firstNumber = 20;  // object data (viz will be added)
+        
+        int sumOfCalc1 = Calculator1.add(20, 7); // passed through the argument
+        System.out.println(sumOfCalc1);
+
+    
+    }
+
+}
+
+//METHOD DEFINITION:
+
+/*
+ * A method is a block of code that performs a specific task. 
+ * It can take zero or more parameters and can return a value or not. 
+ * Methods allow you to group a series of statements to perform a specific task and can be called multiple times within a program.
+ */
+
---
+++ 01-IntroductionToProgramming/Calculator1.java
@@ -0,0 +1,9 @@
+
+public class Calculator1{
+    int firstNumber;
+    int secondNumber;
+
+    public static int add(int val1, int val2){
+        return val1 + val2;
+    }
+}
